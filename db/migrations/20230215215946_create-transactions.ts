8ba2a9b3ba96ed1c02701d2e686c3fbb6597d17f
Gabriel Borel <biel_borel@hotmail.com>
2023-02-15 19:30:27 -0300

    table name fix

--- db/migrations/20230215215946_create-transactions.ts
+++ db/migrations/20230215215946_create-transactions.ts
@@ -1,7 +1,7 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-	await knex.schema.createTable('transacations', (table) => {
+	await knex.schema.createTable('transactions', (table) => {
 		table.uuid('id').primary()
 		table.text('title').notNullable()
 		table.decimal('amount', 10, 2).notNullable()
