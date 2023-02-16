174c0e5fda1b06d14e2b5084e57b3d1c720a4fd8
Gabriel Borel <biel_borel@hotmail.com>
2023-02-15 23:18:15 -0300

    get transactions summary

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -41,4 +41,12 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     return reply.status(200).send({ transaction });
   });
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first();
+
+    return { summary };
+  });
 }
