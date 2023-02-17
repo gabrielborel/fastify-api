3e61cb49d836f78b93396207da4daba707f6300b
Gabriel Borel <biel_borel@hotmail.com>
2023-02-16 23:54:32 -0300

    test: list transaction by id and get summary

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,12 @@
-import { expect, test, beforeAll, afterAll, describe } from 'vitest';
+import {
+  expect,
+  test,
+  beforeAll,
+  afterAll,
+  describe,
+  beforeEach,
+} from 'vitest';
+import { execSync } from 'node:child_process';
 import request from 'supertest';
 import { app } from '../src/app';
 
@@ -9,6 +17,11 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all');
+    execSync('npm run knex migrate:latest');
   });
 
   test('user can create a new transaction', async () => {
@@ -43,5 +56,64 @@ describe('Transactions routes', () => {
         amount: 5000,
       }),
     ]);
+  });
+
+  test('user can list a transaction', async () => {
+    const createTransactionRes = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Any title',
+        amount: 5000,
+        type: 'credit',
+      });
+
+    const cookies = createTransactionRes.get('Set-Cookie');
+
+    const listTransactionsRes = await request(app.server)
+      .get(`/transactions`)
+      .set('Cookie', cookies);
+    const transactionId = listTransactionsRes.body.transactions[0].id;
+
+    const listTransactionByIdRes = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies);
+
+    expect(listTransactionByIdRes.statusCode).toBe(200);
+    expect(listTransactionByIdRes.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'Any title',
+        amount: 5000,
+      })
+    );
+  });
+
+  test('it should be able to get the summary', async () => {
+    const createTransactionRes = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Any credit title',
+        amount: 5000,
+        type: 'credit',
+      });
+
+    const cookies = createTransactionRes.get('Set-Cookie');
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Any debit title',
+        amount: 2000,
+        type: 'debit',
+      });
+
+    const summaryRes = await request(app.server)
+      .get(`/transactions/summary`)
+      .set('Cookie', cookies);
+
+    expect(summaryRes.statusCode).toBe(200);
+    expect(summaryRes.body.summary).toEqual({
+      amount: 3000,
+    });
   });
 });
